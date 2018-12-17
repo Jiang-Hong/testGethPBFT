5b0a627cb70af9d49cfe7a09d84792f7e054afaf
Hjiang <jiang19911102@163.com>
2018-12-17 19:16:30 +0800

    conf genesis

---
+++ genesis.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import json
+
+with open('docker/10.json', 'rb') as f:
+    genesis = json.loads(f.read())
+genesis['config']['chainId'] = 101
+
+newGenesis = json.dumps(genesis, indent=2)
+
+with open('docker/new.json', 'w') as f:
+    print(newGenesis, file=f)
