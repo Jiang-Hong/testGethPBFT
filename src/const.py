f9de4dd4415dd7c720a68b075535d0ec6091c515
Hjiang <jiang19911102@163.com>
2019-09-12 09:43:55 +0800

    1

--- src/const.py
+++ src/const.py
@@ -4,14 +4,13 @@
 import threading
 
 CONFIG = 'config/testLatency2.txt'  # config file for HIBEChain
-USERNAME = 'root'  # username of servers
-PASSWD = 'Blockchain'  # password of servers
+USERNAME = 'u0'  # username of servers
+PASSWD = 'test'  # password of servers
 KEY_FILE = '/home/rkd/.ssh/test/id_rsa'
 # ssh-copy-id -i private_key_file root@...
 # ssh-add /full/path/to/private-key_file
 IMAGE = 'rkdghd/geth-pbft:500'  # docker image name
 MAXPAYLOAD = 15  # maximum number of containers running on one server
-IP_CONFIG = 'config/my_ip.txt'  # server IPs
+IP_CONFIG = 'config/ip.txt'  # server IPs
 SECONDS_IN_A_DAY = 60 * 60 * 24
 SEMAPHORE = threading.BoundedSemaphore(6)
-SEND_TX_SPEED = 120
